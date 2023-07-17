d5eba8e0624c458bf7756971f4575710a0e483dc
jaya4ever <111536082+jaya4ever@users.noreply.github.com>
2023-07-17 13:25:14 -0500

    added the date

--- script.js
+++ script.js
@@ -20,16 +20,16 @@ function calculateBMI() {
   let bmi = (weight / ((height * height) / 10000)).toFixed(2);
 
   if (bmi < 18.6) {
-    result.innerHTML = `You are Under Weight, Start Eating : Your BMI is<span>${bmi}</span>`;
+    result.innerHTML = `You are Under Weight,  at Date ${Date.now()}.toISOString(); : Your BMI is<span>${bmi}</span>`;
     result.style.color = "#ffc44d";
   } else if (bmi >= 18.6 && bmi < 24.9) {
-    result.innerHTML = `Your weight is Normal : Your BMI is <span>${bmi}</span>`;
+    result.innerHTML = `Your weight is Normal, at Date ${new Date().toISOString()} : Your BMI is <span>${bmi}</span>`;
     result.style.color = "#0be881";
   } else if (bmi <= 30) {
-    result.innerHTML = `Your weight is overweight : Your BMI is <span>${bmi}</span>`;
+    result.innerHTML = `Your weight is overweight,at Date ${new Date().toISOString()} : Your BMI is <span>${bmi}</span>`;
     result.style.color = "#ff884d";
   } else {
-    result.innerHTML = `You are obesed : Your BMI is <span>${bmi}</span>`;
+    result.innerHTML = `You are obesed,at Date ${new Date().toISOString()} : Your BMI is <span>${bmi}</span>`;
     result.style.color = "#ff5e57";
   }
 }
